b8cf3fcee5c33ea2c92078d194c157d562fe072f
artemisia000 <misiapierobon@gmail.com>
2022-01-16 12:59:24 +0100

    termine esercizi

--- university.sql
+++ university.sql
@@ -71,27 +71,48 @@ GROUP BY `office_address`;
 
 --3.Calcolare la media dei voti di ogni appello d'esame
 
-
-
+SELECT AVG (`vote`) AS `Media`
+FROM `exam_student`;
 
 --4.Contare quanti corsi di laurea ci sono per ogni dipartimento
 
-
+SELECT COUNT(`degree_id`)
+FROM `courses` 
+INNER JOIN `degrees` ON `courses`.`degree_id`=`degrees`.`id`
+INNER JOIN `departments` ON `degrees`.`id` = `departments`.`id`
+WHERE `departments`.`id`;
 
 -----------------------------------------------------------------------------------------------------------
 
 
 --1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 
+SELECT * 
+FROM `students`
+INNER JOIN `degrees` ON `students`.`degree_id`=`degrees`.`id`
+WHERE `degrees`.`name` LIKE '%Economia%';
 
 
 --2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
+SELECT * 
+FROM `degrees`
+INNER JOIN `departments`ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` LIKE '%Neuroscienze%';
 
 
 --3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
-
+SELECT *
+FROM `teachers` 
+INNER JOIN `courses` ON `teachers`.`id`=`courses`.`id`
+WHERE `teachers`.`name` = 'Fulvio';
 
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 relativo dipartimento in ordine alfabetico per cognome e nome.
+
+SELECT `students`.`name`,`students`.`surname`,`degrees`.`name`,`departments`.`name` 
+FROM `students` 
+INNER JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN`departments` ON `students`.`degree_id` = `departments`.`id`
+ORDER BY `students`.`name`,`students`.`surname` ;
